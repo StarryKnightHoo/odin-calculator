2ff501b4c53c644bea658cf44a23bc4c03687ce4
Howard <howard.zhang0108@gmail.com>
2026-06-27 18:02:49 -0400

    Add reference variables for the remaining buttons

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ let displayNum1 = "";
 let displayNum2 = "";
 let displayOperator = "";
 const result = document.querySelector(".result");
-const one = document.querySelector(".one")
-const two = document.querySelector(".two")
-const three = document.querySelector(".three")
-const four = document.querySelector(".four")
-const five = document.querySelector(".five")
-const six = document.querySelector(".six")
-const seven = document.querySelector(".seven")
-const eight = document.querySelector(".eight")
-const nine = document.querySelector(".nine")
-const zero = document.querySelector(".zero")
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const plus = document.querySelector(".plus");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+
+
+one.addEventListener("click", );
 
 function add(num1, num2) {
     return num1 + num2;
@@ -42,5 +51,4 @@ function operate(operator, num1, num2) {
 }
 
 function buttonClickNum() {
-    .addEventListener
 }
