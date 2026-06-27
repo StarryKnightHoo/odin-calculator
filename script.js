ded5b9db9b88409472c9d3c4d235b8aed3eaad79
Howard <howard.zhang0108@gmail.com>
2026-06-27 19:58:51 -0400

    Fix calculator operation
    Calculator considered num1 and num2 variables
    to be strings still, so I converted them to
    numbers when they are being operated on in the
    operator functions.

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ const result = document.querySelector(".result");
 const equals = document.querySelector("equals");
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(operator, num1, num2) {
@@ -27,9 +27,9 @@ function operate(operator, num1, num2) {
         return add(num1, num2);
     } else if (operator == "-") {
         return subtract(num1, num2);
-    } else if (operator == "*") {
+    } else if (operator == "*" || operator == "×") {
         return multiply(num1, num2)
-    } else if (operator == "/") {
+    } else if (operator == "/" || operator == "÷") {
         return divide(num1, num2)
     }
 }
