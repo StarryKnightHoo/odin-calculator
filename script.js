c6432cb79bdc8110d99b50fe22c047b6c67e5ebb
Howard <howard.zhang0108@gmail.com>
2026-06-28 15:45:26 -0400

    Fix divide by zero message
    Change divide by zero message to a pop up and
    reset the calculator because you could perform
    operations on the divide by zero message
    previously which caused errors.

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ function divide(num1, num2) {
     if (num2 != 0) {
         return Number(num1) / Number(num2);
     } else {
-        return "Don't Do That!"
+        alert("Don't Do That!");
+        return 0;
     }
 
 }
