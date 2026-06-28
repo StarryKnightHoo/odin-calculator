1ca0e558bd87f8ce5015ab8e7d9a14eb641ebd6f
Howard <howard.zhang0108@gmail.com>
2026-06-28 15:37:18 -0400

    Add divide by zero message
    When the user tries to divide something by zero,
    they will be greet by a message to not do that.

--- script.js
+++ script.js
@@ -22,7 +22,12 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return Number(num1) / Number(num2);
+    if (num2 != 0) {
+        return Number(num1) / Number(num2);
+    } else {
+        return "Don't Do That!"
+    }
+
 }
 
 function operate(operator, num1, num2) {
